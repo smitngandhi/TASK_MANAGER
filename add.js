fff4249e3b12211d4dfe72b5e4c6da3c62557c2c
Smit Gandhi <smitgandhi585@gmail.com>
2023-11-07 13:00:17 +0530

    Fourth Commit

--- add.js
+++ add.js
@@ -7,10 +7,17 @@
     let description = document.getElementById('description').value;
     let date = document.getElementById('date').value;
     let statuss = document.getElementById('statusselect').value;
+    if(localStorage.getItem(`${user}title${tasknumber}`)=="Your Title will be shown here"||localStorage.getItem(`${user}title${tasknumber}`)==null)
+    {
         localStorage.setItem(`${user}title${tasknumber}`,title);
         localStorage.setItem(`${user}description${tasknumber}`,description);
         localStorage.setItem(`${user}deadline${tasknumber}`,date);
         localStorage.setItem(`${user}status${tasknumber}`,statuss);
+    }
+    else
+    {
+        alert("Task Already Exists")
+    }
     }
 
 
