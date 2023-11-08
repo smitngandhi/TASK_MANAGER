f5e5150e2c9562d19ed61d116e4719d7011c37cc
Smit Gandhi <smitgandhi585@gmail.com>
2023-11-08 09:07:26 +0530

    Fifth Commit

--- add.js
+++ add.js
@@ -1,18 +1,26 @@
 
     function getall()
     {
+        let date = new Date();
+        let month  = date.getMonth() + 1;
+        let day = date.getDate();
+        let year = date.getFullYear();
+
+        let fulldate = `${year}-${month}-${day}`;
+
     let user = document.getElementById('opselect').value;
     let tasknumber = document.getElementById('numbers').value;
     let title = document.getElementById('title').value;
     let description = document.getElementById('description').value;
-    let date = document.getElementById('date').value;
+    let deadline = document.getElementById('date').value;
     let statuss = document.getElementById('statusselect').value;
     if(localStorage.getItem(`${user}title${tasknumber}`)=="Your Title will be shown here"||localStorage.getItem(`${user}title${tasknumber}`)==null)
     {
         localStorage.setItem(`${user}title${tasknumber}`,title);
         localStorage.setItem(`${user}description${tasknumber}`,description);
-        localStorage.setItem(`${user}deadline${tasknumber}`,date);
+        localStorage.setItem(`${user}deadline${tasknumber}`,deadline);
         localStorage.setItem(`${user}status${tasknumber}`,statuss);
+        localStorage.setItem(`${user}created${tasknumber}`,fulldate);
     }
     else
     {
--- edit.js
+++ edit.js
@@ -1,16 +1,30 @@
 //console.log(localStorage.getItem(`User1deadline1`));
 
-  function setall()
+  function viewall()
 {
-    
  let user = document.getElementById('opselect').value;
  let task = document.getElementById('tasknumber').value;
 let titletext = document.getElementById('titletext');
     let descriptiontext = document.getElementById('descriptiontext');
+    let createdtext = document.getElementById('createdtext');
     let deadlinetext = document.getElementById('deadlinetext');
     let statustext = document.getElementById('statustext');
+    if(localStorage.getItem(`${user}title${task}`)=="Your Title will be shown here"||localStorage.getItem(`${user}title${task}`)=="null"||localStorage.getItem(`${user}title${task}`)==null)
+    {
+        localStorage.setItem(`${user}title${task}`,"Your Title will be shown here");
+ localStorage.setItem(`${user}description${task}`,"Your Description will be shown here");
+ localStorage.setItem(`${user}deadline${task}`,"Your Deadline will be shown here");
+ localStorage.setItem(`${user}status${task}`,"Your Status will be shown here");
+ localStorage.setItem(`${user}created${task}`,"Your Date of creating the task will be shown here");
+ titletext.innerHTML = localStorage.getItem(`${user}title${task}`);    
+    descriptiontext.innerHTML = localStorage.getItem(`${user}description${task}`);
+    createdtext.innerHTML = localStorage.getItem(`${user}created${task}`);
+    deadlinetext.innerText = localStorage.getItem(`${user}deadline${task}`);
+    statustext.innerText = localStorage.getItem(`${user}status${task}`);
+    }
     titletext.innerHTML = localStorage.getItem(`${user}title${task}`);    
     descriptiontext.innerHTML = localStorage.getItem(`${user}description${task}`);
+    createdtext.innerHTML = localStorage.getItem(`${user}created${task}`);
     deadlinetext.innerText = localStorage.getItem(`${user}deadline${task}`);
     statustext.innerText = localStorage.getItem(`${user}status${task}`);
 }
@@ -19,14 +33,22 @@ function edittask()
 {
     let user = document.getElementById('opselect').value;
  let task = document.getElementById('tasknumber').value;
+    if(localStorage.getItem(`${user}title${task}`)=="Your Title will be shown here"||localStorage.getItem(`${user}title${task}`)=="null"||localStorage.getItem(`${user}title${task}`)==null)
+    {
+        alert("Add Your Task First");
+    }
+    else
+    {
     let title = prompt("Re enter your title");
+
     let description = prompt("Re enter your description");
-    let date = prompt("Re enter your deadline");
+    let date = prompt("Re enter your deadline","Year-Month-Day");
     let status = prompt("Re enter your status");
     localStorage.setItem(`${user}title${task}`,title);
         localStorage.setItem(`${user}description${task}`,description);
         localStorage.setItem(`${user}deadline${task}`,date);
         localStorage.setItem(`${user}status${task}`,status);
+    }
 }
 
 function removetask()
@@ -37,6 +59,7 @@ function removetask()
  localStorage.setItem(`${user}description${task}`,"Your Description will be shown here");
  localStorage.setItem(`${user}deadline${task}`,"Your Deadline will be shown here");
  localStorage.setItem(`${user}status${task}`,"Your Status will be shown here");
+ localStorage.setItem(`${user}created${task}`,"Your Date of creating the task will be shown here");
 }
 
 
